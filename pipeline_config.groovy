b78a06a99e18078108d42f9a2533c497f39e6920
Oleg 'livelace' Popov <o.popov@livelace.ru>
2022-08-17 20:17:47 +0300

    descrease same ratio

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -6,7 +6,7 @@ libraries {
         cluster = "k8s-1"
         namespace = "task"
 
-        image = "harbor-core.k8s-2.livelace.ru/infra/tools:latest"
+        image = "harbor-core.k8s-2.livelace.ru/infra/tools:22.04"
 
         volume = """
             task-gosquito-public-conf, data/gosquito/conf, rw

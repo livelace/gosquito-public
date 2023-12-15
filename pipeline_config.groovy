902d4a368ddfdeec755c0f610056693bdd6d77ad
Oleg 'livelace' Popov <o.popov@livelace.ru>
2023-12-15 12:28:09 +0300

    rss: add ncomms

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -7,7 +7,6 @@ libraries {
         namespace = "gosquito"
 
         image = "harbor-core.k8s-2.livelace.ru/portage/gosquito-update:1.0.0"
-        force_pull = true
 
         volume = """
             gosquito-public-conf, data/gosquito/conf, rw
